d08a44c4ea0ffb608f9170aac7d04b9356520cb9
AndysCarc <anderson.carcamo@catolica.edu.sv>
2023-03-21 12:25:23 -0600

    vista producto

--- views/Food/index.php
+++ views/Food/index.php
@@ -1,42 +1,41 @@
 <?php require_once VIEW_LAYOUT_PATH . 'header.php' ?>
 <link rel="stylesheet" href="<?= URL . "public/css/food.css" ?>">
+<link rel="stylesheet" href="<?= URL . "public/css/cardfood.css" ?>">
 <!-- INSERTAR CONTENIDO -->
 
-<head>
-    <title>Productos</title>
-</head>
-<body>
-    <h1>Listado de productos</h1>
-   
-    <table>
-        <tr>
-        <th>Imagen</th>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Precio</th>
-            
-            <th>Cantidad</th>
-      <th>Acciones</th>
-        </tr>
-        <?php foreach ($viewData['productos'] as $producto) { ?>
-    <tr>
-        <td><img src="<?php echo $producto->getImageUrl(); ?>" alt="<?php echo $producto->getNombre(); ?>" style="width: 25px;"></td>
-        <td><?php echo $producto->getIdProducto(); ?></td>
-        <td><?php echo $producto->getNombre(); ?></td>
-        <td><?php echo $producto->getPrecio(); ?></td>
-        <td>
-          <form action="<?=URL?>carrito/agregarProducto" method="post">
-            <input type="hidden" name="id_producto" value="<?php echo $producto->getIdProducto(); ?>">
-            <input type="number" name="cantidad" value="1" min="1" max="10">
-            
-        </td>
-        <td>
-            <button type="submit">Agregar al carrito</button>
-          </form>
-        </td>
-    </tr>
-<?php } ?>
 
+<body>
+    <div class="container pt-2">
+
+        <div class="products">
+            
+            <div class="product">
+                <div class="image">
+                    <img src="public/img/earphone.png" alt="">
+                </div>
+                <div class="namePrice">
+                    <h3>earphone</h3>
+                    <span>$ 15.99</span>
+                </div>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit..</p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <div class="bay">
+                    <button> bay now </button>
+                </div>
+            </div>
+            
+
+            
+
+            
+        </div>
+    </div>
 
         
 
