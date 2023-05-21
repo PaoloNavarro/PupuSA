543b439c117905b27c8594b6ffc80695b44e26d8
andyscarc <anderson.carcamo@catolica.edu.sv>
2023-05-21 11:52:31 -0500

    vista de editar promocion

--- views/Admin/promocionese.php
+++ views/Admin/promocionese.php
@@ -1,19 +1,32 @@
 <?php require_once VIEW_LAYOUT_PATH . 'header.php' ?>
 
-
 <body>
-    <h1>Editar promoción</h1>
-    <form method="post" action="guardar_promocion.php" enctype="multipart/form-data">
-        <label>Título:</label><br>
-        <input type="text" name="titulo"><br><br>
+    <div class="container">
+        <div class="row">
+        <div class="col-8">
+            <h1 class="mb-4">Editar promoción</h1>
+            <form method="post" action="guardar_promocion.php" enctype="multipart/form-data">
 
-        <label>Imagen:</label><br>
-        <input type="file" name="imagen"><br><br>
+                <div class="form-group col-md-12 mb-4" >
+                    <label for="titulo">Título:</label>
+                    <input type="text" name="titulo" class="form-control">
+                </div>
 
-        <label>Precio:</label><br>
-        <input type="number" name="precio"><br><br>
+                <div class="form-group col-md-12 mb-4">
+                    <label for="imagen">Imagen:</label>
+                    <input type="file" name="imagen" class="form-control-file">
+                </div>
 
-        <input type="submit" value="Guardar">
-    </form>
+                <div class="form-group col-md-12 mb-3">
+                    <label for="precio">Precio:</label>
+                    <input type="number" name="precio" class="form-control">
+                </div>
+
+                <input type="submit" value="Guardar" class="btn btn-primary">
+            </form>
+        </div>
+        </div>
+    </div>
 </body>
 <?php require_once VIEW_LAYOUT_PATH . 'header.php' ?>
+
