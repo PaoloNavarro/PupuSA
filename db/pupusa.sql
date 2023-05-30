534a2ef70080342cb720236017cc919de6fee0ed
andyscarc <anderson.carcamo@catolica.edu.sv>
2023-05-30 00:28:06 -0600

    actualizacion a db

--- db/pupusa.sql
+++ db/pupusa.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2023 a las 07:48:06
+-- Tiempo de generación: 30-05-2023 a las 08:02:45
 -- Versión del servidor: 10.4.28-MariaDB
 -- Versión de PHP: 8.2.4
 
@@ -20,6 +20,111 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `pupusa`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `carrito`
+--
+
+CREATE TABLE `carrito` (
+  `id_carrito` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `fecha_agregado` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `categoria`
+--
+
+CREATE TABLE `categoria` (
+  `id_categoria` int(11) NOT NULL,
+  `descripcion` varchar(50) NOT NULL,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `categoria`
+--
+
+INSERT INTO `categoria` (`id_categoria`, `descripcion`, `estado`) VALUES
+(1, 'Desayuno', 2),
+(2, 'Almuerzo', 1),
+(3, 'Cena', 1),
+(4, 'bebida', 1),
+(5, 'Promoción', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalle_pedido`
+--
+
+CREATE TABLE `detalle_pedido` (
+  `id_detalle` int(11) NOT NULL,
+  `id_producto` int(11) NOT NULL,
+  `id_pedido` int(11) NOT NULL,
+  `cantidad` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `detalle_pedido`
+--
+
+INSERT INTO `detalle_pedido` (`id_detalle`, `id_producto`, `id_pedido`, `cantidad`) VALUES
+(18, 1, 53, 1),
+(19, 2, 53, 1),
+(20, 1, 54, 1),
+(21, 2, 54, 1),
+(22, 2, 55, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_pedido`
+--
+
+CREATE TABLE `estado_pedido` (
+  `id_estado_pedido` int(11) NOT NULL,
+  `Estado` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `estado_pedido`
+--
+
+INSERT INTO `estado_pedido` (`id_estado_pedido`, `Estado`) VALUES
+(1, 'En proceso'),
+(2, 'Finalizado'),
+(3, 'Cancelado');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pedidos`
+--
+
+CREATE TABLE `pedidos` (
+  `id_pedido` int(11) NOT NULL,
+  `total_pagar` double NOT NULL,
+  `fecha_pedido` date NOT NULL,
+  `id_estado_pedido` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `ubicacion` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `pedidos`
+--
+
+INSERT INTO `pedidos` (`id_pedido`, `total_pagar`, `fecha_pedido`, `id_estado_pedido`, `id_usuario`, `ubicacion`) VALUES
+(53, 0.9, '2023-04-09', 2, 41, 'Bo sanjacitno'),
+(54, 0.9, '2023-05-30', 2, 45, 'asssssssssssssssss'),
+(55, 0.4, '2023-05-30', 1, 45, 'asssssssssssssssss');
 
 -- --------------------------------------------------------
 
@@ -48,9 +153,92 @@ INSERT INTO `producto` (`id_producto`, `nombre`, `precio`, `image_url`, `descrip
 (11, 'Camaron', 1.25, 'http://localhost/pupuSA/uploads/64758d708fa377.38488034.jpg', 'Pruébalas ya, riquísimas pupusas de camarón ', 2, 1),
 (12, 'Loca', 1.85, 'http://localhost/pupuSA/uploads/64758d9eeec651.56838752.jpg', 'Deléitate con nuestra pupusa loca ', 3, 1);
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `roles`
+--
+
+CREATE TABLE `roles` (
+  `id_rol` int(11) NOT NULL,
+  `rol` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `roles`
+--
+
+INSERT INTO `roles` (`id_rol`, `rol`) VALUES
+(1, 'admin'),
+(2, 'cliente');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `nombre_usuario` varchar(50) NOT NULL,
+  `apellido_usuario` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `telefono` varchar(11) NOT NULL,
+  `direccion` varchar(150) NOT NULL,
+  `sexo` varchar(10) NOT NULL,
+  `rol` int(11) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish2_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `nombre_usuario`, `apellido_usuario`, `correo`, `telefono`, `direccion`, `sexo`, `rol`, `password`) VALUES
+(40, 'pedro', 'nelo', 'piopiopaolo12@gmail.com', '64355641', 'Bo sanjacinto', 'Femenino', 1, '$2y$10$F7733xlg0JplvC6qkJ8ubuo64vFg6kV6gLoD4pVoEb4SBM1sRQwfO'),
+(41, 'paolo', 'Navarro', 'paolo@gmail.com', '64355641', 'Bo sanjacitno', 'Masculino', 2, '$2y$10$8/EIFpLz5mM52qMCv8neLOR75fPJ.nOInZZciSVRZrTHucUbZZGnq'),
+(42, 'Diego', 'Hernandez', 'diego@gmail.com', '77823546', 'Snat ana', 'Masculino', 1, '$2y$10$eNkRbidPzPmVyePdUyMKP.jHy9jzt//UPLGMcflCL7rVocCah5Ntu'),
+(43, 'martin', 'sanabria', 'martinSanabr@gmail.com', '75248965', 'barrio san benito', 'Masculino', 2, '$2y$10$hQC7mxpd1p1LoPXlG6aTd.K.8ugcKjE.xmAqVVNwTgA00tgPvSB5S'),
+(44, 'andy', 'carcamo', 'abc@gmail.com', '79050501', 'asssssssss', 'Masculino', 1, '$2y$10$Liri53f3hKiUwbU.k5wllO5NrAzhu/nLbgCvAc32CJJM7BJnLoIiC'),
+(45, 'ander', 'carcamo', 'abcd@gmail.com', '79050501', 'asssssssss', 'Masculino', 2, '$2y$10$F7N19MB.PKB2HBNgOOQMlerjiKzKYzAxeAEngNC1W2WliGPOiC3bW');
+
 --
 -- Índices para tablas volcadas
 --
+
+--
+-- Indices de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  ADD PRIMARY KEY (`id_carrito`);
+
+--
+-- Indices de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  ADD PRIMARY KEY (`id_categoria`);
+
+--
+-- Indices de la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  ADD PRIMARY KEY (`id_detalle`),
+  ADD KEY `id_producto` (`id_producto`),
+  ADD KEY `id_pedido` (`id_pedido`);
+
+--
+-- Indices de la tabla `estado_pedido`
+--
+ALTER TABLE `estado_pedido`
+  ADD PRIMARY KEY (`id_estado_pedido`);
+
+--
+-- Indices de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD PRIMARY KEY (`id_pedido`),
+  ADD KEY `id_estado_pedido` (`id_estado_pedido`),
+  ADD KEY `id_usuario` (`id_usuario`);
 
 --
 -- Indices de la tabla `producto`
@@ -60,8 +248,51 @@ ALTER TABLE `producto`
   ADD KEY `categoria` (`categoria`);
 
 --
+-- Indices de la tabla `roles`
+--
+ALTER TABLE `roles`
+  ADD PRIMARY KEY (`id_rol`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`),
+  ADD KEY `rol` (`rol`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
+
+--
+-- AUTO_INCREMENT de la tabla `carrito`
+--
+ALTER TABLE `carrito`
+  MODIFY `id_carrito` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `categoria`
+--
+ALTER TABLE `categoria`
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  MODIFY `id_detalle` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_pedido`
+--
+ALTER TABLE `estado_pedido`
+  MODIFY `id_estado_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT de la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  MODIFY `id_pedido` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=56;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
@@ -70,14 +301,46 @@ ALTER TABLE `producto`
   MODIFY `id_producto` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
+-- AUTO_INCREMENT de la tabla `roles`
+--
+ALTER TABLE `roles`
+  MODIFY `id_rol` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+
+--
 -- Restricciones para tablas volcadas
 --
+
+--
+-- Filtros para la tabla `detalle_pedido`
+--
+ALTER TABLE `detalle_pedido`
+  ADD CONSTRAINT `detalle_pedido_ibfk_3` FOREIGN KEY (`id_pedido`) REFERENCES `pedidos` (`id_pedido`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detalle_pedido_ibfk_4` FOREIGN KEY (`id_producto`) REFERENCES `producto` (`id_producto`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `pedidos`
+--
+ALTER TABLE `pedidos`
+  ADD CONSTRAINT `pedidos_ibfk_1` FOREIGN KEY (`id_estado_pedido`) REFERENCES `estado_pedido` (`id_estado_pedido`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `pedidos_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `producto`
 --
 ALTER TABLE `producto`
   ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`categoria`) REFERENCES `categoria` (`id_categoria`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`rol`) REFERENCES `roles` (`id_rol`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
